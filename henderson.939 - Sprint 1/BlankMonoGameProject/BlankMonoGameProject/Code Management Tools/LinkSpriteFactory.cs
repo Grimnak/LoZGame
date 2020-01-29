679e7d32062a067c298c69bd12ceb16832676c38
Grimnak <36608889+Grimnak@users.noreply.github.com>
2020-01-29 15:11:16 -0500

    Add files via upload
    Initial upload of files.

---
+++ henderson.939 - Sprint 1/BlankMonoGameProject/BlankMonoGameProject/Code Management Tools/LinkSpriteFactory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LoZClone
+{
+    public class LinkSpriteFactory
+    {
+        private Texture2D linkIdleLeftTexture;
+        //private SpriteSheetData linkIdleLeftData = new SpriteSheetData("Link_Idle_Left", 25, 25);
+        private Texture2D linkIdleRightTexture;
+        //private SpriteSheetData linkIdleRightData = new SpriteSheetData("Link_Idle_Right", 25, 25);
+
+        private static LinkSpriteFactory instance = new LinkSpriteFactory();
+
+        public static LinkSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private LinkSpriteFactory()
+        {
+        }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            //linkIdleLeftTexture = content.Load<Texture2D>(linkIdleLeftData.GetFilePath);
+        }
+
+        public Sprite CreateSprite_Link_Idle_Left()
+        {
+            return new Sprite(linkIdleLeftTexture, linkIdleLeftData);
+        }
+
+        public UnanimatedSprite CreateSprite_Link_Idle_Right()
+        {
+            return new UnanimatedSprite(linkIdleRightTexture, linkIdleRightData);
+        }
+    }
+}
---
+++ henderson.939 - Sprint 1/BlankMonoGameProject/BlankMonoGameProject/Interfaces/ISprite.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LoZClone
+{
+    public interface ISprite
+    {
+        void Update();
+        void Draw(SpriteBatch spriteBatch, Color spriteTint);
+    }
+}
