d3f7d3ae6aee65d730bf7dbacc63668b448ef3cb
Grimnak <36608889+Grimnak@users.noreply.github.com>
2020-02-10 12:04:58 -0500

    Add files via upload

---
+++ henderson.939 - Sprint 1/BlankMonoGameProject/Sprites/LinkSpriteClasses/LinkIdleUpSprite.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LoZClone
+{
+    public class LinkIdleUpSprite : ISprite
+    {
+        private Texture2D spriteSheet;
+        private int spriteSheetRows, spriteSheetColumns;
+        private int currentFrame = 0;
+
+        public LinkIdleUpSprite(Texture2D spriteTexture, SpriteSheetData data)
+        {
+            spriteSheet = spriteTexture;
+            spriteSheetRows = data.Rows;
+            spriteSheetColumns = data.Columns;
+        }
+
+        public void Update()
+        {
+            if (currentFrame > 0)
+            {
+                currentFrame = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location, Color spriteTint)
+        {
+            int width = spriteSheet.Width / spriteSheetColumns;
+            int height = spriteSheet.Height / spriteSheetRows;
+            int row = (int)((float)currentFrame / (float)spriteSheetColumns);
+            int column = currentFrame % spriteSheetColumns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Draw(spriteSheet, destinationRectangle, sourceRectangle, spriteTint);
+        }
+    }
+}
