f7e629baade849605b2f8396b05feadf638550f7
Grimnak <36608889+Grimnak@users.noreply.github.com>
2020-02-10 11:57:49 -0500

    Add files via upload

---
+++ henderson.939 - Sprint 1/BlankMonoGameProject/LoZGame.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LoZClone
+{
+    public class LoZGame : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        public SpriteBatch SpriteBatch
+        {
+            get { return spriteBatch; }
+        }
+        private IPlayer link;
+        private CommandLoader commandLoader;
+        private IController keyboardController;
+
+        public LoZGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+        }
+        protected override void Initialize()
+        {
+            link = new Link(this);
+            commandLoader = new CommandLoader(this, link);
+            keyboardController = new KeyboardController(this, commandLoader);
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            LinkSpriteFactory.Instance.LoadAllTextures(Content);
+        }
+        protected override void UnloadContent()
+        {
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            keyboardController.Update();
+            link.Update();
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Gray);
+
+            spriteBatch.Begin();
+            link.Draw();
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
