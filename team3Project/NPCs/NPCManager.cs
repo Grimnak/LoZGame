e15cb8d4a6eab1ccd321b282379bfd2e3ec79208
Grimnak <36608889+Grimnak@users.noreply.github.com>
2020-02-11 15:09:02 -0500

    Update NPCManager.cs

--- team3Project/NPCs/NPCManager.cs
+++ team3Project/NPCs/NPCManager.cs
@@ -13,20 +13,20 @@ namespace LoZClone
         private Array<IEnemySprite> addNPCs()
         {
             NPCs = new Array<IEnemySprite>();
-            NPCs.SetValue(new Dodongo(), 1);
-            NPCs.SetValue(new Dragon(), 2);
-            NPCs.SetValue(new Stalfos(), 3);
-            NPCs.SetValue(new Keese(), 4);
-            NPCs.SetValue(new Gel(), 5);
-            NPCs.SetValue(new Zol(), 6);
-            NPCs.SetValue(new Goriya(), 7);
-            NPCs.SetValue(new Rope(), 8);
-            NPCs.SetValue(new SpikeCross(), 9);
-            NPCs.SetValue(new WallMaster(), 10);
+            NPCs.SetValue(new Dodongo(), 0);
+            NPCs.SetValue(new Dragon(), 1);
+            NPCs.SetValue(new Stalfos(), 2);
+            NPCs.SetValue(new Keese(), 3);
+            NPCs.SetValue(new Gel(), 4);
+            NPCs.SetValue(new Zol(), 5);
+            NPCs.SetValue(new Goriya(), 6);
+            NPCs.SetValue(new Rope(), 7);
+            NPCs.SetValue(new SpikeCross(), 8);
+            NPCs.SetValue(new WallMaster(), 9);
 
-           // NPCs.SetValue(new OldMan(), 11);
-           // NPCs.SetValue(new Merchant(), 12);
-           // NPCs.SetValue(new Flame(), 13);
+           // NPCs.SetValue(new OldMan(), 10);
+           // NPCs.SetValue(new Merchant(), 11);
+           // NPCs.SetValue(new Flame(), 12);
         }
 
         public IEnemySprite cycleRight()
