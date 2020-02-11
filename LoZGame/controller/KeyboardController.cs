fe6a83b008233cb9d10b28badca0b7f96b254014
Grimnak <36608889+Grimnak@users.noreply.github.com>
2020-02-11 15:45:14 -0500

    Update KeyboardController.cs

--- LoZGame/controller/KeyboardController.cs
+++ LoZGame/controller/KeyboardController.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace LoZClone
 {
-    class KeyboardController : IController
+    public class KeyboardController : IController
     {
         LoZGame game;
-        CommandLoader loader;
-        Dictionary<Keys, ICommand> commands;
+        CommandLoader allCommands;
 
         private ICommand currentCommand;
 
-        public KeyboardController(LoZGame game, CommandLoader loader)
+        public KeyboardController(LoZGame game, CommandLoader allCommands)
         {
             this.game = game;
-            this.loader = loader;
-            commands = this.loader.getCommands;
+            this.allCommands = allCommands;
         }
 
 
@@ -32,17 +25,93 @@ namespace LoZClone
 
             if (pressed.Length == 0)
             {
-                currentCommand = loader.getIdle;
+                currentCommand = allCommands.getIdle;
                 currentCommand.execute();
             }
             else
             {
-                foreach (Keys key in pressed)
+
+                if (pressed.Contains(Keys.Z))
                 {
-                    if (commands.ContainsKey(key))
-                    {
-                        commands[key].execute();
-                    }
+                    currentCommand = allCommands.getZ;
+                    currentCommand.execute();
+                }
+                else if (pressed.Contains(Keys.N))
+                {
+                    currentCommand = allCommands.getN;
+                    currentCommand.execute();
+                }
+                else if (pressed.Contains(Keys.W))
+                {
+                    currentCommand = allCommands.getW;
+                    currentCommand.execute();
+                }
+                else if (pressed.Contains(Keys.A))
+                {
+                    currentCommand = allCommands.getA;
+                    currentCommand.execute();
+                }
+                else if (pressed.Contains(Keys.S))
+                {
+                    currentCommand = allCommands.getS;
+                    currentCommand.execute();
+                }
+                else if (pressed.Contains(Keys.D))
+                {
+                    currentCommand = allCommands.getD;
+                    currentCommand.execute();
+                }
+                //maybe change
+                else if (pressed.Contains(Keys.D1))
+                {
+                    currentCommand = allCommands.getOne;
+                    currentCommand.execute();
+                }
+                //maybe change
+                else if (pressed.Contains(Keys.D2))
+                {
+                    currentCommand = allCommands.getTwo;
+                    currentCommand.execute();
+                }
+                //maybe change
+                else if (pressed.Contains(Keys.E))
+                {
+                    currentCommand = allCommands.getE;
+                    currentCommand.execute();
+                }
+
+                if (pressed.Contains(Keys.U))
+                {
+                    currentCommand = allCommands.getU;
+                    currentCommand.execute();
+                }
+                else if (pressed.Contains(Keys.I))
+                {
+                    currentCommand = allCommands.getI;
+                    currentCommand.execute();
+                }
+
+                if (pressed.Contains(Keys.O))
+                {
+                    currentCommand = allCommands.getO;
+                    currentCommand.execute();
+                }
+                else if (pressed.Contains(Keys.P))
+                {
+                    currentCommand = allCommands.getP;
+                    currentCommand.execute();
+                }
+
+                if (pressed.Contains(Keys.Q))
+                {
+                    currentCommand = allCommands.getQ;
+                    currentCommand.execute();
+                }
+
+                if (pressed.Contains(Keys.R))
+                {
+                    currentCommand = allCommands.getR;
+                    currentCommand.execute();
                 }
             }
         }
