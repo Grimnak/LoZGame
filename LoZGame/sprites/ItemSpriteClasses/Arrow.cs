3643577c129c404145034114d0d3edf61ceeacfb
arehfeldt <16rehfeldta@gmail.com>
2020-02-13 16:42:09 -0500

    added arrow functionality but it doesnt really work yet

--- LoZGame/sprites/ItemSpriteClasses/Arrow.cs
+++ LoZGame/sprites/ItemSpriteClasses/Arrow.cs
@@ -15,6 +15,8 @@ namespace LoZClone
         private int lifeTime;
         private int scale;
         private string direction;
+        private bool moving;
+        private float rotation;
         public Vector2 location { get; set; }
         public Arrow(Texture2D texture, Vector2 loc, int scale)
         {
@@ -23,6 +25,9 @@ namespace LoZClone
             lifeTime = 0;
             location = loc;
             this.scale = scale;
+            this.moving = false;
+            this.direction = "Up";
+            this.rotation = 0;
         }
 
         public Arrow(Texture2D texture, Vector2 loc, string direction, int scale)
@@ -33,20 +38,49 @@ namespace LoZClone
             location = loc;
             this.scale = scale;
             this.direction = direction;
+            this.moving = true;
+            if (this.direction == "Up")
+            {
+                location = new Vector2(loc.X, loc.Y - 64);
+                rotation = 0;
+            } else if (this.direction == "Left")
+            {
+                location = new Vector2(loc.X - 64, loc.Y);
+                rotation = MathHelper.PiOver2;
+            } else if (this.direction == "Right")
+            {
+                location = new Vector2(loc.X + 64, loc.Y);
+                rotation = MathHelper.Pi;
+            } else
+            {
+                location = new Vector2(loc.X, loc.Y + 64);
+                rotation = 3*MathHelper.PiOver2;
+            }
         }
         public void Update()
         {
             lifeTime++;
-            if (lifeTime > 20)
+            if (this.direction == "Up" && this.moving == true)
             {
-                lifeTime = 0;
+                this.location = new Vector2(location.X, location.Y - 1);
+            }
+            else if (this.direction == "Left" && this.moving == true)
+            {
+                this.location = new Vector2(location.X - 1, location.Y);
+            }
+            else if (this.direction == "Right" && this.moving == true)
+            {
+                this.location = new Vector2(location.X + 1, location.Y);
+            }
+            else if (this.direction == "Down" && this.moving == true)
+            {
+                this.location = new Vector2(location.X, location.Y + 1);
             }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            Rectangle dest = new Rectangle((int)location.X, (int)location.Y, frame.Width * scale, frame.Height * scale);
-            spriteBatch.Draw(Texture, dest, frame, Color.White);
+            spriteBatch.Draw(Texture, location, frame, Color.White, rotation, location, scale, SpriteEffects.None, 0f);
         }
 
     }
