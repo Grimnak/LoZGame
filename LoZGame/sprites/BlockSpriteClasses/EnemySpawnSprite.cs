17da73ca6f5a7d3f7bec6de23b0cdde7f3397c27
Sammy Lisa <lisa.5@buckeyemail.osu.edu>
2020-02-15 21:11:39 -0500

    commit

---
+++ LoZGame/sprites/BlockSpriteClasses/EnemySpawnSprite.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LoZClone
+{
+    public class EnemySpawnSprite : ISprite
+    {
+        private Texture2D spriteSheet;
+        private int spriteSheetRows, spriteSheetColumns;
+        private int currentFrame, totalFrames;
+        private int currentUpdate;
+        private int updatesPerFrame = 5;
+        private int scale;
+
+        public EnemySpawnSprite(Texture2D spriteTexture, SpriteSheetData data, int scale)
+        {
+            spriteSheet = spriteTexture;
+            currentFrame = 0;
+            spriteSheetRows = data.Rows;
+            spriteSheetColumns = data.Columns;
+            totalFrames = spriteSheetRows * spriteSheetColumns;
+            this.scale = scale;
+        }
+
+        public void Update()
+        {
+            currentUpdate++;
+            if (currentUpdate == updatesPerFrame)
+            {
+                currentUpdate = 0;
+                currentFrame++;
+                if (currentFrame == totalFrames)
+                    currentFrame = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location, Color spriteTint)
+        {
+            int width = spriteSheet.Width / spriteSheetColumns;
+            int height = spriteSheet.Height / spriteSheetRows;
+            int row = (int)((float)currentFrame / (float)spriteSheetColumns);
+
+            Rectangle sourceRectangle = new Rectangle(0, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width * scale, height * scale);
+
+            spriteBatch.Draw(spriteSheet, destinationRectangle, sourceRectangle, spriteTint);
+        }
+    }
+}
