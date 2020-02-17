cb54c5082212c26e3bf22a0c5648b109e6e9637b
Grimnak <henderson.939@osu.edu>
2020-02-17 02:36:28 -0500

    new link state folder

---
+++ LoZGame/player/PlayerStates/DieState.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace LoZClone
+{
+    public class DieState : IPlayerState
+    {
+        private LoZGame game;
+        private IPlayer player;
+        private ISprite sprite;
+
+        public DieState(LoZGame game, IPlayer playerInstance)
+        {
+            this.game = game;
+            player = playerInstance;
+            player.IsDead = true;
+            sprite = createCorrectSprite();
+        }
+        private ISprite createCorrectSprite()
+        {
+            return LinkSpriteFactory.Instance.createSpriteLinkDie(player.CurrentColor);
+        }
+        public void idle()
+        {
+        }
+        public void moveUp()
+        {
+        }
+        public void moveDown()
+        {
+        }
+        public void moveLeft()
+        {
+        }
+        public void moveRight()
+        {
+        }
+        public void attack()
+        {
+        }
+        public void die()
+        {
+        }
+        public void pickupItem(int itemTime)
+        {
+        }
+        public void useItem(int waitTime)
+        {
+        }
+        public void Update()
+        {
+            sprite.Update();
+        }
+        public void Draw()
+        {
+            sprite.Draw(game.SpriteBatch, player.CurrentLocation, player.CurrentTint);
+        }
+    }
+}
