3f07929617d16755cb0df447c89378b68930ce06
Grimnak <36608889+Grimnak@users.noreply.github.com>
2020-02-16 19:13:35 -0500

    Create CommandLoader.cs

---
+++ Code Reviews/Sprint 2/Readability Reviews/CommandLoader.cs
@@ -0,0 +1,19 @@
+# Code Readability Review
+
+### Author: Eric Henderson.939
+
+### Date: 02/16/2020
+
+### Sprint No.: Sprint 2
+
+### File Name: LoZGame/controller/Commands/CommandLoader.cs
+
+### File Author Name: Jeremy Wensink.27
+
+### Time Taken to Review: 30 minutes
+
+###  Comments on What is Readable
+- The dictionary as it is adding all of the various commands to it is extremely recognizable and clean.
+- The command classes are all named something very intuitive and are easy to understand.
+- The get properties are aptly named getIdle and getDict, clearly indicating which private field they serve.
+- Overall, this file and code structure are very well-named and factored very well.  Someone who has never seen this code before could easily understand everything it does without much time invested.
