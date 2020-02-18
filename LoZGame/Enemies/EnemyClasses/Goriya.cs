647a1a6230797d8e77b67f90fdaa92824aa9ec79
Grimnak <henderson.939@osu.edu>
2020-02-17 23:08:49 -0500

    goriya fix

--- LoZGame/Enemies/EnemyClasses/Goriya.cs
+++ LoZGame/Enemies/EnemyClasses/Goriya.cs
@@ -6,12 +6,6 @@
 
     public class Goriya : IEnemy
     {
-        public bool HasBoomerang
-        {
-            get { return this.hasBoomerang; }
-            set { this.hasBoomerang = value; }
-        }
-
         private IEnemyState currentState;
         private int health = 10;
         private int coolDown;
