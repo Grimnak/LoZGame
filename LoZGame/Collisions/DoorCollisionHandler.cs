8cd415212de0a66e55b52e4f77e9146724263c29
wensinkj <wensinkj99@gmail.com>
2020-03-22 14:37:51 -0400

    added puzzle door functionality

--- LoZGame/Collisions/DoorCollisionHandler.cs
+++ LoZGame/Collisions/DoorCollisionHandler.cs
@@ -16,26 +16,30 @@
         {
             if ((this.door.State is UnlockedDoorState || this.door.State is BombedDoorState) && (!(player.State is GrabbedState) && !(player.State is IdleState)))
             {
-              if (door.Physics.Location == door.LeftScreenLoc)
-              {
-                  LoZGame.Instance.Dungeon.MoveLeft();
-              }
-              else if (door.Physics.Location == door.RightScreenLoc)
-              {
-                  LoZGame.Instance.Dungeon.MoveRight();
-              }
-              else if (door.Physics.Location == door.DownScreenLoc)
-              {
-                  LoZGame.Instance.Dungeon.MoveDown();
-              }
-              else if (door.Physics.Location == door.UpScreenLoc)
-              {
-                  LoZGame.Instance.Dungeon.MoveUp();
-              }
-            } 
+                if (door.Physics.Location == door.LeftScreenLoc)
+                {
+                    LoZGame.Instance.Dungeon.MoveLeft();
+                }
+                else if (door.Physics.Location == door.RightScreenLoc)
+                {
+                    LoZGame.Instance.Dungeon.MoveRight();
+                }
+                else if (door.Physics.Location == door.DownScreenLoc)
+                {
+                    LoZGame.Instance.Dungeon.MoveDown();
+                }
+                else if (door.Physics.Location == door.UpScreenLoc)
+                {
+                    LoZGame.Instance.Dungeon.MoveUp();
+                }
+            }
             else if (this.door.State is LockedDoorState && player.HasKey)
             {
                 player.HasKey = false;
+                this.door.Open();
+            }
+            else if (this.door.State is PuzzleDoorState && ((PuzzleDoorState)this.door.State).IsSolved)
+            {
                 this.door.Open();
             }
         }
