c89333c4b2116081416d427c91aa180cc1b2d98a
arehfeldt <16rehfeldta@gmail.com>
2020-03-23 16:15:35 -0400

    refactored enemy projectile manager

--- LoZGame/Managers/EnemyProjectileManager.cs
+++ LoZGame/Managers/EnemyProjectileManager.cs
@@ -53,54 +53,6 @@
             }
         }
 
-        /*public void AddDragonFireballs(Dragon dragon)
-        {
-            this.projectileId++;
-            this.listSize++;
-            this.projectileList.Add(this.projectileId, new FireballProjectile(dragon.Physics.Location, new Vector2(-3, -1)));
-            this.projectileId++;
-            this.listSize++;
-            this.projectileList.Add(this.projectileId, new FireballProjectile(dragon.Physics.Location, new Vector2(-3, 0)));
-            this.projectileId++;
-            this.listSize++;
-            this.projectileList.Add(this.projectileId, new FireballProjectile(dragon.Physics.Location, new Vector2(-3, 1)));
-        }
-
-        public void AddOldManFireballs(OldMan oldMan, IPlayer player)
-        {
-            Vector2 leftFireBlockLocation = new Vector2(oldMan.Physics.Location.X - 100, oldMan.Physics.Location.Y + 20);
-            Vector2 rightFireBlockLocation = new Vector2(oldMan.Physics.Location.X + 160, oldMan.Physics.Location.Y + 20);
-
-            this.projectileId++;
-            this.listSize++;
-            this.projectileList.Add(this.projectileId, new FireballProjectile(leftFireBlockLocation, DetermineOldManLeftFireballVelocity(oldMan, player)));
-            this.projectileId++;
-            this.listSize++;
-            this.projectileList.Add(this.projectileId, new FireballProjectile(rightFireBlockLocation, DetermineOldManRightFireballVelocity(oldMan, player)));
-        }
-
-        private Vector2 DetermineOldManLeftFireballVelocity(OldMan oldMan, IPlayer player)
-        {
-            Vector2 leftFireBlockLocation = new Vector2(oldMan.Physics.Location.X - 100, oldMan.Physics.Location.Y + 20);
-            float leftBlockDiffX = player.Physics.Location.X - leftFireBlockLocation.X;
-            float leftBlockDiffY = player.Physics.Location.Y - leftFireBlockLocation.Y;
-
-            float leftBlockDiffTotal = (float)Math.Sqrt(Math.Pow(leftBlockDiffX, 2) + Math.Pow(leftBlockDiffY, 2));
-
-            return new Vector2(leftBlockDiffX / leftBlockDiffTotal * 2, leftBlockDiffY / leftBlockDiffTotal * 2);
-        }
-
-        private Vector2 DetermineOldManRightFireballVelocity(OldMan oldMan, IPlayer player)
-        {
-            Vector2 rightFireBlockLocation = new Vector2(oldMan.Physics.Location.X + 100, oldMan.Physics.Location.Y + 20);
-            float rightBlockDiffX = player.Physics.Location.X - rightFireBlockLocation.X;
-            float rightBlockDiffY = player.Physics.Location.Y - rightFireBlockLocation.Y;
-
-            float rightBlockDiffTotal = (float)Math.Sqrt(Math.Pow(rightBlockDiffX, 2) + Math.Pow(rightBlockDiffY, 2));
-
-            return new Vector2(rightBlockDiffX / rightBlockDiffTotal * 2, rightBlockDiffY / rightBlockDiffTotal * 2);
-        }*/
-
         public void Remove(int instance)
         {
             this.projectileList.Remove(instance);
