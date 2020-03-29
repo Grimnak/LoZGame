d096db5ad11ea6962b02e9dd0d5932a2832963cc
wensinkj <wensinkj99@gmail.com>
2020-03-29 12:33:15 -0400

    added game states

---
+++ LoZGame/GameStates/PlayGameState.cs
@@ -0,0 +1,51 @@
+﻿namespace LoZClone
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PlayGameState : IGameState
+    {
+        public void Death()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inventory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TitleScreen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TransitionRoom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WinGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LoZGame/interfaces/IGameState.cs
@@ -0,0 +1,52 @@
+﻿namespace LoZClone
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IGameState
+    {
+        /// <summary>
+        /// Game state goes to title screen.
+        /// </summary>
+        void TitleScreen();
+
+        /// <summary>
+        /// Game state goes to game in progress.
+        /// </summary>
+        void PlayGame();
+
+        /// <summary>
+        /// Game state goes to player death.
+        /// </summary>
+        void Death();
+
+        /// <summary>
+        /// Game state goes to access inventory.
+        /// </summary>
+        void Inventory();
+
+        /// <summary>
+        /// Game state goes to win state.
+        /// </summary>
+        void WinGame();
+
+        /// <summary>
+        /// Game state for room transitions.
+        /// </summary>
+        void TransitionRoom();
+
+        /// <summary>
+        /// Updates the current state.
+        /// </summary>
+        void Update();
+
+        /// <summary>
+        /// Draws the objects associated with the current state.
+        /// </summary>
+        void Draw();
+    }
+}
