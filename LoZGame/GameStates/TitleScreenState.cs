f83787cd98d0915447dae0829a1a180089bd6975
wensinkj <wensinkj99@gmail.com>
2020-03-30 17:01:09 -0400

    title screen is epic now

--- LoZGame/GameStates/TitleScreenState.cs
+++ LoZGame/GameStates/TitleScreenState.cs
@@ -1,6 +1,7 @@
 ﻿namespace LoZClone
 {
     using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -81,6 +82,7 @@
         public void Draw()
         {
             // TODO
+            LoZGame.Instance.SpriteBatch.DrawString(LoZGame.Instance.Font, "TITLE SCREEN - PRESS ENTER", new Vector2(100, 100), Color.White, 0.0f, new Vector2(0, 0), 1.0f, SpriteEffects.None, 1f);
         }
     }
 }
