ede6232dc2f9fca4bb97ac46bedc3af9a8ad95e9
Sammy Lisa <lisa.5@buckeyemail.osu.edu>
2020-03-30 18:21:28 -0400

    rebase

---
+++ LoZGame/PlayGameState.cs
@@ -0,0 +1,82 @@
+﻿namespace LoZClone
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class PlayGameState : IGameState
+    {
+
+        public PlayGameState()
+        {
+
+        }
+
+        public void Death()
+        {
+            LoZGame.Instance.GameState = new DeathState();
+        }
+
+        public void Inventory()
+        {
+            LoZGame.Instance.GameState = new InventoryState();
+        }
+
+        public void PlayGame()
+        {
+            // Can't transition into a state you are already in.
+        }
+
+        public void TitleScreen()
+        {
+            LoZGame.Instance.GameState = new TitleScreenState();
+        }
+
+        public void TransitionRoom()
+        {
+            LoZGame.Instance.GameState = new TransitionRoomState();
+        }
+
+        public void WinGame()
+        {
+            LoZGame.Instance.GameState = new WinGameState();
+        }
+
+        public void Update()
+        {
+            // TODO
+            foreach (IPlayer player in LoZGame.Instance.Players)
+            {
+                player.Update();
+            }
+            LoZGame.Instance.GameObjects.Update();
+            LoZGame.Instance.CollisionDetector.Update(LoZGame.Instance.Players.AsReadOnly(), LoZGame.Instance.GameObjects.Enemies.EnemyList.AsReadOnly(), LoZGame.Instance.GameObjects.Blocks.BlockList.AsReadOnly(), LoZGame.Instance.GameObjects.Doors.DoorList.AsReadOnly(), LoZGame.Instance.GameObjects.Items.ItemList.AsReadOnly(), LoZGame.Instance.GameObjects.Entities.PlayerProjectiles.AsReadOnly(), LoZGame.Instance.GameObjects.Entities.EnemyProjectiles.AsReadOnly());
+
+        }
+
+        public void Draw()
+        {
+            // TODO
+            if (LoZGame.Instance.Dungeon.CurrentRoomX != 1 || LoZGame.Instance.Dungeon.CurrentRoomY != 1)
+            {
+                LoZGame.Instance.SpriteBatch.Draw(LoZGame.Instance.Background, new Rectangle(0, 0, 800, 480), new Rectangle(0, 0, 236, 160), LoZGame.Instance.DungeonTint, 0.0f, new Vector2(0, 0), SpriteEffects.None, 0f);
+            }
+
+            LoZGame.Instance.GameObjects.Draw();
+
+            if (LoZGame.Instance.Dungeon.CurrentRoomX == 0 && LoZGame.Instance.Dungeon.CurrentRoomY == 2)
+            {
+                LoZGame.Instance.SpriteBatch.DrawString(LoZGame.Instance.Font, LoZGame.Instance.Dungeon.CurrentRoom.RoomText, new Vector2(100, 100), Color.White, 0.0f, new Vector2(0, 0), 1.0f, SpriteEffects.None, 1f);
+            }
+
+            foreach (IPlayer player in LoZGame.Instance.Players)
+            {
+                player.Draw();
+            }
+        }
+    }
+}
---
+++ LoZGame/TitleScreenState.cs
@@ -0,0 +1,105 @@
+﻿namespace LoZClone
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TitleScreenState : IGameState
+    {
+        public TitleScreenState()
+        {
+            LoZGame.Instance.GameObjects.Clear();
+            LoZGame.Instance.Players.Clear();
+
+            LoZGame.Instance.Dungeon = new Dungeon(1);
+            LoZGame.Instance.CollisionDetector = new CollisionDetection(LoZGame.Instance.Dungeon);
+
+            LoZGame.Instance.Players.Add(new Link(new Vector2(
+                    (float)(BlockSpriteFactory.Instance.HorizontalOffset + (BlockSpriteFactory.Instance.TileWidth * 5.5)),
+                    (float)(BlockSpriteFactory.Instance.VerticalOffset + (BlockSpriteFactory.Instance.TileHeight * 6)))));
+
+            LoZGame.Instance.Dungeon.Player = LoZGame.Instance.Players[0];
+
+
+            KeyboardCommandLoader keyboardLoader = new KeyboardCommandLoader(LoZGame.Instance.Players[0]);
+            MouseCommandLoader mouseLoader = new MouseCommandLoader();
+            if (LoZGame.Instance.Controllers.Count == 0)
+            {
+                LoZGame.Instance.Controllers.Add(new KeyboardController(keyboardLoader));
+
+
+                LoZGame.Instance.Controllers.Add(new MouseController(mouseLoader));
+            }
+            else
+            {
+                for (int i = 0; i < LoZGame.Instance.Controllers.Count; i++)
+                {
+                    if (LoZGame.Instance.Controllers[i] is KeyboardController)
+                    {
+                        LoZGame.Instance.Controllers[i] = new KeyboardController(keyboardLoader);
+                    }
+                    else if (LoZGame.Instance.Controllers[i] is MouseController)
+                    {
+                        LoZGame.Instance.Controllers[i] = new MouseController(mouseLoader);
+                    }
+                }
+            }
+        }
+
+        public void Death()
+        {
+            // Can't die on title screen.
+        }
+
+        public void Inventory()
+        {
+            // Can't access inventory on title screen.
+        }
+
+        public void PlayGame()
+        {
+            LoZGame.Instance.GameState = new PlayGameState();
+        }
+
+        public void TitleScreen()
+        {
+            // Can do a hard reset while in this state already.
+            LoZGame.Instance.GameState = new TitleScreenState();
+        }
+
+        public void TransitionRoom()
+        {
+            // Can't transition room from title screen.
+        }
+
+        public void WinGame()
+        {
+            // Can't win game from the title screen.
+        }
+
+        public void Update()
+        {
+            // TODO update title screen image
+
+
+            for (int i = 0; i < LoZGame.Instance.Controllers.Count; i++)
+            {
+                LoZGame.Instance.Controllers[i].Update();
+            }
+
+            ///temporary reset
+            //CommandReset temp = new CommandReset(LoZGame.Instance.Players[0]);
+            //temp.Execute();
+        }
+
+        public void Draw()
+        {
+            // TODO
+            LoZGame.Instance.SpriteBatch.DrawString(LoZGame.Instance.Font, "TITLE SCREEN - PRESS ENTER", new Vector2(100, 100), Color.White, 0.0f, new Vector2(0, 0), 1.0f, SpriteEffects.None, 1f);
+        }
+    }
+}
