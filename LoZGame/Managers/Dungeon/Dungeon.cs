35204093be5bb7ac1f0143a2b83ec0e90181dbcc
wensinkj <wensinkj99@gmail.com>
2020-04-10 16:56:39 -0400

    minimap initial files

---
+++ LoZGame/Managers/Dungeon/Dungeon.cs
@@ -0,0 +1,90 @@
+﻿namespace LoZClone
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    /// Manager for all dungeon rooms.
+    /// </summary>
+    public partial class Dungeon
+    {
+        private List<List<Room>> dungeonLayout;
+        private int currentX;
+        private int currentY;
+        private int maxX;
+        private int maxY;
+        private IPlayer player;
+        private string currentDungeonFile;
+        private int dungeonNumber;
+
+        public IPlayer Player
+        {
+            get { return player; }
+            set { this.player = value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dungeon"/> class.
+        /// </summary>
+        /// <param name="dungeonNumber">Number of the dungeon whose file is to be parsed.</param>
+        public Dungeon(int dungeonNumber)
+        {
+            this.dungeonNumber = dungeonNumber;
+            this.currentDungeonFile = "../../../../../etc/levels/dungeon" + this.dungeonNumber + ".xml";
+
+
+            switch (this.dungeonNumber)
+            {
+                case 1:
+                    LoZGame.Instance.DungeonTint = Color.White;
+                    this.currentX = 2;
+                    this.currentY = 5; // player spawns at curX/curY
+                    this.maxX = 6;
+                    this.maxY = 6;
+                    break;
+                case 2:
+                    LoZGame.Instance.DungeonTint = Color.Yellow;
+                    this.currentX = 1;
+                    this.currentY = 7; // player spawns at curX/curY
+                    this.maxX = 4;
+                    this.maxY = 8;
+                    break;
+                // more cases here for more dungeons
+                default:
+                    break;
+            }
+
+            this.dungeonLayout = XMLHandler.Parse(this.currentDungeonFile);
+
+            this.LoadNewRoom();
+        }
+
+        public int CurrentRoomX
+        {
+            get { return currentX; }
+            set { currentX = value; }
+        }
+
+        public int CurrentRoomY
+        {
+            get { return currentY; }
+            set { currentY = value; }
+        }
+
+        public Room CurrentRoom
+        {
+            get { return this.dungeonLayout[this.currentY][this.currentX]; }
+        }
+
+        public int DungeonNumber { get { return dungeonNumber; } }
+
+        /*
+         * Given a particular X and Y value, return that room
+         */
+        public Room GetRoom(int Y, int X)
+        {
+            return this.dungeonLayout[Y][X];
+        }
+    }
+}
---
+++ LoZGame/Managers/Dungeon/MiniMapRoom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace LoZClone
+{
+    public class MiniMapRoom
+    {
+        private ISprite sprite;
+        private bool exists;
+        private bool visited;
+        private Vector2 location;
+
+        public MiniMapRoom(bool exists, ISprite sprite, int y, int x)
+        {
+            this.exists = exists;
+            this.sprite = sprite;
+            this.visited = false;
+
+            this.location = new Vector2(x,y);
+        }
+
+        public void Explore()
+        {
+            this.visited = true;
+        }
+
+
+        public void Draw()
+        {
+            if (this.visited)
+            {
+                this.sprite.Draw(this.location, LoZGame.Instance.DefaultTint, 1.0f);
+            }
+        }
+
+
+    }
+}
