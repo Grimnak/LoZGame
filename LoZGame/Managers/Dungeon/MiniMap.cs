21c9ec32f1f426cbde19e6b61ed6c8da07457edc
arehfeldt <16rehfeldta@gmail.com>
2020-04-11 12:15:57 -0400

    hopefully the starts of a working dynamic minimap that can load every dungeon

--- LoZGame/Managers/Dungeon/MiniMap.cs
+++ LoZGame/Managers/Dungeon/MiniMap.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LoZClone
+﻿namespace LoZClone
 {
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Xna.Framework;
+
     public class MiniMap
     {
         private List<List<MiniMapRoom>> dungeonLayout;
+        private Vector2 mapSize = new Vector2(400, 240);
+        private Vector2 roomSize;
+        private Point location;
+
+        public Point Location { get; set; }
+
+        public enum DoorLocation
+        {
+            North, 
+            South, 
+            East, 
+            West
+        }
 
         public MiniMap()
         {
@@ -32,15 +43,62 @@ namespace LoZClone
             {
                 for (int j = 0; j < dungeonLayout[i].Count; j++)
                 {
-                    this.dungeonLayout[i][j].Draw();
+                    this.dungeonLayout[i][j].Draw(this.location, this.roomSize.ToPoint());
                 }
             }
+        }
+
+        private List<DoorLocation> FetchDoors(Room room)
+        {
+            List<MiniMap.DoorLocation> doors = new List<MiniMap.DoorLocation>();
+            foreach (Door door in room.Doors)
+            {
+                if (!(door.State is HiddenDoorState))
+                {
+                    switch (door.GetLoc())
+                    {
+                        case "N":
+                            doors.Add(MiniMap.DoorLocation.North);
+                            break;
+                        case "S":
+                            doors.Add(MiniMap.DoorLocation.South);
+                            break;
+                        case "E":
+                            doors.Add(MiniMap.DoorLocation.East);
+                            break;
+                        case "W":
+                            doors.Add(MiniMap.DoorLocation.West);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return doors;
+        }
+
+        public void LoadMap(List<List<Room>> dungeon, int maxX, int maxY)
+        {
+            this.dungeonLayout.Clear();
+            int roomY = 0, roomX = 0;
+            while (roomY < maxY)
+            {
+                while (roomX < maxX)
+                {
+                    if (dungeon[roomX][roomY].Exists)
+                    {
+                        List<MiniMap.DoorLocation> doors = FetchDoors(dungeon[roomX][roomY]);
+                        this.dungeonLayout[roomX][roomY] = new MiniMapRoom(roomX, roomY, doors);
+                    }
+                }
+            }
+            this.roomSize = this.mapSize / Math.Max(roomX, roomY);
         }
 
         // Note to grader, we are aware this is some smelly code, but it wasn't apparent to us how to do it any cleaner.
         public void CreateDungeonOneMiniMap()
         {
-            this.dungeonLayout[0][0] = new MiniMapRoom(false, null, 0, 0);
+            /*this.dungeonLayout[0][0] = new MiniMapRoom(false, null, 0, 0);
             this.dungeonLayout[0][1] = new MiniMapRoom(true, null, 0, 1);
             this.dungeonLayout[0][2] = new MiniMapRoom(true, null, 0, 2);
             this.dungeonLayout[0][3] = new MiniMapRoom(false, null, 0, 3);
@@ -75,7 +133,7 @@ namespace LoZClone
             this.dungeonLayout[5][2] = new MiniMapRoom(true, null, 5, 2);
             this.dungeonLayout[5][3] = new MiniMapRoom(true, null, 5, 3);
             this.dungeonLayout[5][4] = new MiniMapRoom(false, null, 5, 4);
-            this.dungeonLayout[5][5] = new MiniMapRoom(false, null, 5, 5);
+            this.dungeonLayout[5][5] = new MiniMapRoom(false, null, 5, 5);*/
         }
 
         public void CreateDungeonTwoMiniMap()
--- LoZGame/Managers/Dungeon/MiniMapRoom.cs
+++ LoZGame/Managers/Dungeon/MiniMapRoom.cs
@@ -4,23 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace LoZClone
 {
     public class MiniMapRoom
     {
+        private static readonly Color BackgroundColor = Color.SandyBrown;
+        private static readonly Color RoomColor = Color.SaddleBrown;
         private ISprite sprite;
         private bool exists;
         private bool visited;
         private Vector2 location;
 
-        public MiniMapRoom(bool exists, ISprite sprite, int y, int x)
+        public MiniMapRoom(int x, int y, List<MiniMap.DoorLocation> doors)
         {
-            this.exists = exists;
-            this.sprite = sprite;
             this.visited = false;
-
             this.location = new Vector2(x,y);
+            CreateSprite();
+        }
+
+        private void CreateSprite()
+        {
+            Texture2D roomSprite = new Texture2D(LoZGame.Instance.GraphicsDevice, 5, 5, false, SurfaceFormat.Color);
         }
 
         public void Explore()
@@ -28,15 +34,12 @@ namespace LoZClone
             this.visited = true;
         }
 
-
-        public void Draw()
+        public void Draw(Point startLoc, Point roomSize)
         {
             if (this.visited)
             {
                 this.sprite.Draw(this.location, LoZGame.Instance.DefaultTint, 1.0f);
             }
         }
-
-
     }
 }
