515514dce0ab91f5ae81d986a71e11b31f8f38e8
Sammy Lisa <lisa.5@buckeyemail.osu.edu>
2020-04-13 09:29:17 -0400

    rebase

---
+++ LoZGame/Enemies/EnemyStates/KeeseStates/SpawnKeeseState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace LoZClone
+{
+    class SpawnKeeseState : KeeseEssentials, IEnemyState
+    {
+        public SpawnKeeseState(IEnemy enemy)
+        {
+            this.Enemy = enemy;
+            this.Sprite = EnemySpriteFactory.Instance.EnemySpawn();
+            Console.WriteLine("Spawned!");
+            this.Enemy.CurrentState = this;
+            this.Enemy.Physics.MovementVelocity = Vector2.Zero;
+        }
+
+        public override void Update()
+        {
+            this.Enemy.UpdateState();
+        }
+    }
+}
