95860aeae507d68e95e8c1e3657d3ab6fe0d2334
ryan scott <scott.2081@su.edu>
2020-04-13 22:09:35 -0400

    tile numbers

---
+++ LoZGame/util/DataConstants/RoomData/RoomConstants.cs
@@ -0,0 +1,41 @@
+﻿namespace LoZClone
+{
+    public struct RoomConstants
+    {
+        private const float BlockDepth= .2f;
+        private const int BlockHeightOffset = 8;
+        private const float TileD = .001f;
+
+        public float BlockTileDepth => BlockDepth;
+
+        public int BlockTileHeightOffset => BlockHeightOffset;
+
+        public float TileDepth => TileD;
+
+/*
+        public string WaterTileStr => WaterTile;
+
+        public string BasementBrickTileStr => BasementBrickTileStr;
+
+        public string FireGapTileStr => FireGapTile;
+
+        public string TurqouiseStatueLeftStr => TurqouiseStatueLeftStr;
+
+        public string TurqouiseStatueRightStr => TurquoiseStatueRight;
+
+        public string MovableSquare2Str => MovableSquare2;
+
+        public string BlueStatueLeftStr => BlueStatueLeft;
+
+        public string BlueStatueRightStr => BlueStatueRight;
+
+        public string BlueStatueLeftStr2 => BlueStatueLeft2;
+
+        public string BlueStatueRightStr2 => BlueStatueRight2;
+
+        public string OrangeStatueLeftStr2 => OrangeStatueLeft2;
+
+        public string OrangeStatueRightStr2 => OrangeStatueRight2;
+        */
+    }
+}
