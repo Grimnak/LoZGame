87b6c9e1151979b4da8d85448b7f853196dc64a2
arehfeldt <16rehfeldta@gmail.com>
2020-04-20 16:56:52 -0400

    Manhandla not yet fixed

--- LoZGame/Enemies/EnemyClasses/ManhandlaBody.cs
+++ LoZGame/Enemies/EnemyClasses/ManhandlaBody.cs
@@ -9,17 +9,21 @@
     {
         private bool spawnedChildren;
         List<IEnemy> heads;
+        bool test;
 
         public ManhandlaBody(Vector2 location)
         {
+            Console.WriteLine("Passed Loc: " + location);
             this.RandomStateGenerator = new RandomStateGenerator(this);
             this.States = new Dictionary<RandomStateGenerator.StateType, int>(GameData.Instance.EnemyStateWeights.ManhandlaBodyStateList);
             this.Health = new HealthManager(GameData.Instance.EnemyHealthConstants.ManhandlaBodyHealth);
             this.Physics = new Physics(location);
+            Console.WriteLine("Physics Loc: " + Physics.Location);
             this.Physics.Mass = GameData.Instance.EnemyMassConstants.DragonMass;
             this.Physics.IsMoveable = false;
-            this.CurrentState = new IdleEnemyState(this);
             this.Physics.Bounds = new Rectangle((int)this.Physics.Location.X, (int)this.Physics.Location.Y, EnemySpriteFactory.GetEnemyWidth(this), EnemySpriteFactory.GetEnemyHeight(this));
+            this.CurrentState = new IdleEnemyState(this);
+            Console.WriteLine("Bounds: " + Physics.Bounds);
             this.EnemyCollisionHandler = new EnemyCollisionHandler(this);
             this.Expired = false;
             this.IsTransparent = true;
@@ -31,10 +35,28 @@
             this.AI = EnemyAI.Manhandla;
             this.MinMaxWander = new Point(LoZGame.Instance.UpdateSpeed, LoZGame.Instance.UpdateSpeed * 2);
             this.IsSpawning = false;
+            test = false;
         }
 
         public override void OnCollisionResponse(ICollider otherCollider, CollisionDetection.CollisionSide collisionSide)
         {
+        }
+
+        public override void OnCollisionResponse(int sourceWidth, int sourceHeight, CollisionDetection.CollisionSide collisionSide)
+        {
+            base.OnCollisionResponse(sourceWidth, sourceHeight, collisionSide);
+            switch (collisionSide)
+            {
+                case CollisionDetection.CollisionSide.Top:
+                case CollisionDetection.CollisionSide.Bottom:
+                    this.Physics.MovementVelocity = new Vector2(this.Physics.MovementVelocity.X, -this.Physics.MovementVelocity.Y);
+                    break;
+                case CollisionDetection.CollisionSide.Left:
+                case CollisionDetection.CollisionSide.Right:
+                    this.Physics.MovementVelocity = new Vector2(-this.Physics.MovementVelocity.X, this.Physics.MovementVelocity.Y);
+                    break;
+
+            }
         }
 
         public override void AddChild()
@@ -60,6 +82,10 @@
 
         public override void Update()
         {
+            if (!test)
+            {
+                Console.WriteLine(this.Physics.Bounds);
+            }
             int Health = 0;
             foreach (IEnemy head in this.heads)
             {
