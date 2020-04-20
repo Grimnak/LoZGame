a072dae3b9d0b40d1c26c70ad9be5de6ead4e550
Grimnak <henderson.939@osu.edu>
2020-04-20 00:08:48 -0400

    gibdo rebalancing

--- LoZGame/util/DataConstants/EnemyData/EnemyDamageConstants.cs
+++ LoZGame/util/DataConstants/EnemyData/EnemyDamageConstants.cs
@@ -20,7 +20,7 @@
         private const int RedDarknutDmg = 2;
         private const int BlueDarknutDmg = 4;
         private const int BubbleDmg = 0;
-        private const int GibdoDmg = 4;
+        private const int GibdoDmg = 8;
 
         public int StalfosDamage => StalfosDmg;
 
