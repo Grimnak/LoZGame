9012ac6c88f6e69a03127ddad82f3d15651e9a6c
wensinkj <wensinkj99@gmail.com>
2020-04-24 17:06:54 -0400

    added my code reviews

--- LoZGame/LoZGame.cs
+++ LoZGame/LoZGame.cs
@@ -11,7 +11,7 @@
         public static bool DebugMode = false; // show collision bounding boxes
         public static bool Cheats = true; // infinite life and item uses
         public static bool Music = false;  // Title screen and dungeon music (not SFX)
-        public static bool Laser = false; // changes attacks to laser attack
+        public static bool Laser = true; // changes attacks to laser attack
         public static readonly int StartDungeon = 1; // dungeon ID to load into [1 - 6];
         public int Difficulty = 0; // -1 => EASY 0 => NORMAL 1 => HARD 3 => NIGHTMARE
         private const int DefaultUpdateSpeed = 60;
